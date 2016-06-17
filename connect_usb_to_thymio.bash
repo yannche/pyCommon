18bfc00739235a7e578008309e96313d0ce0b4bf
yannche <yann_che2@yahoo.fr>
2016-06-17 15:20:32 +0200

    tail

--- connect_usb_to_thymio.bash
+++ connect_usb_to_thymio.bash
@@ -1,7 +1,7 @@
 #!/bin/bash
 killall asebamedulla >& /dev/null
 rm -f /tmp/aseba.log ; touch /tmp/aseba.log
-asebamedulla ser:name=Thymio-II &> /tmp/aseba.log &
+nohup asebamedulla ser:name=Thymio-II &> /tmp/aseba.log &
 tail -f /tmp/aseba.log&
 sleep 3
 
