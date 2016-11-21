5736c0b13e8b815537e501816f6e882875142050
yannche <yann_che2@yahoo.fr>
2016-11-21 12:49:25 +0100

    Add files via upload

---
+++ motors2.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+import pythymio2
+import time
+
+with pythymio2.thymio(["buttons","motor","prox"],[]) as Thym:
+    temps = time.time()
+    cnt   = 0
+    def dispatch(evtid, evt_name, evt_args):
+        global temps,cnt
+
+        cnt +=1
+        if cnt > 2000:
+            pythymio2.arrete(Thym)
+            Thym.stop()
+
+        pythymio2.tg(Thym)
+        if evt_name == 'fwd.prox' and time.time() > temps + 0.1:
+            temps = time.time()
+            print(evt_name,evt_args)
+
+    # Now lets start the loopy thing
+    Thym.loop(dispatch)
+    print "Sayonara"
